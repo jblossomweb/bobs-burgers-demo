4155dd65193aee075ad89c4d4b7611f59dc740e3
John Blossom <jblossom.web@gmail.com>
2022-03-06 10:13:42 -0700

    use button on 404 page

--- src/app/pages/NotFoundPage/NotFoundPage.tsx
+++ src/app/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Button from 'app/components/atoms/Button'
 import HeroTemplate from 'app/templates/HeroTemplate'
 
 const NotFoundPage: React.FC = () => (
   <HeroTemplate title="404: Not Found">
     <h1>Sorry! That page was not found.</h1>
     <p>
-      <Link to="/">Back to Home</Link>
+      <Link to="/">
+        <Button>Back to Home</Button>
+      </Link>
     </p>
   </HeroTemplate>
 )
