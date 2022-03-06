1dc2b53caba2563a4dff5ad54813828b7b524292
John Blossom <jblossom.web@gmail.com>
2022-03-06 10:18:41 -0700

    change HomePage

--- src/app/pages/HomePage/HomePage.test.tsx
+++ src/app/pages/HomePage/HomePage.test.tsx
@@ -2,8 +2,23 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import HomePage from './HomePage'
 
-test('renders learn react link', () => {
-  render(<HomePage />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+describe('app/pages/HomePage', () => {
+  it('should render main logo', () => {
+    render(<HomePage />)
+    const imageElement = screen.getByAltText(/bob's burgers/i)
+    expect(imageElement).toBeInTheDocument()
+    expect(imageElement.getAttribute('src')).toEqual('/bobsburgers.png')
+  })
+
+  it('should render characters link', () => {
+    render(<HomePage />)
+    const linkElement = screen.getByText(/characters/i)
+    expect(linkElement).toBeInTheDocument()
+  })
+
+  it('should render episodes link', () => {
+    render(<HomePage />)
+    const linkElement = screen.getByText(/episodes/i)
+    expect(linkElement).toBeInTheDocument()
+  })
 })
--- src/app/pages/HomePage/HomePage.tsx
+++ src/app/pages/HomePage/HomePage.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import logo from 'svgs/logo.svg'
 import HeroTemplate from 'app/templates/HeroTemplate'
+import Button from 'app/components/atoms/Button'
 import styles from './HomePage.module.css'
 
 const HomePage: React.FC = () => (
   <HeroTemplate title="Home Page">
-    <img src={logo} className={styles.logo} alt="logo" />
-    <p>
-      Edit <code>src/app/pages/HomePage</code> and save to reload.
-    </p>
-    <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-      Learn React
-    </a>
+    <h1>
+      <img src="/bobsburgers.png" className={styles.logo} alt="Bob's Burgers" />
+    </h1>
+    <div className={styles.buttons}>
+      <Button>Characters</Button>
+      <Button>Episodes</Button>
+    </div>
   </HeroTemplate>
 )
 
