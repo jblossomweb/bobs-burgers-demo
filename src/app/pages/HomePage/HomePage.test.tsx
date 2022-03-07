3ea86ef4fef40692826f86dc31b6d3b93b54f4b7
John Blossom <jblossom.web@gmail.com>
2022-03-07 10:30:54 -0700

    router decorator for HomePage tests

--- src/app/pages/HomePage/HomePage.test.tsx
+++ src/app/pages/HomePage/HomePage.test.tsx
@@ -1,23 +1,36 @@
 import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
 import { render, screen } from '@testing-library/react'
 import HomePage from './HomePage'
 
 describe('app/pages/HomePage', () => {
   it('should render main logo', () => {
-    render(<HomePage />)
+    render(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>
+    )
     const imageElement = screen.getByAltText(/bob's burgers/i)
     expect(imageElement).toBeInTheDocument()
     expect(imageElement.getAttribute('src')).toEqual('/bobsburgers.png')
   })
 
   it('should render characters link', () => {
-    render(<HomePage />)
+    render(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>
+    )
     const linkElement = screen.getByText(/characters/i)
     expect(linkElement).toBeInTheDocument()
   })
 
   it('should render episodes link', () => {
-    render(<HomePage />)
+    render(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>
+    )
     const linkElement = screen.getByText(/episodes/i)
     expect(linkElement).toBeInTheDocument()
   })
