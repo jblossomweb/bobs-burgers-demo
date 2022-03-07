011f4f6a70e1bf2bc3a443e01563d653b03e6a86
John Blossom <jblossom.web@gmail.com>
2022-03-07 10:36:24 -0700

    routes for new pages

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,20 @@ import React from 'react'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 
 import HomePage from 'app/pages/HomePage'
-import CharactersPage from './pages/CharactersPage'
-import NotFoundPage from './pages/NotFoundPage'
+import NotFoundPage from 'app/pages/NotFoundPage'
+import CharactersPage from 'app/pages/CharactersPage'
+import CharacterPage from 'app/pages/CharacterPage'
+import EpisodesPage from 'app/pages/EpisodesPage'
+import EpisodePage from 'app/pages/EpisodePage'
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="/characters" element={<CharactersPage />} />
+      <Route path="/characters/:id" element={<CharacterPage />} />
+      <Route path="/episodes" element={<EpisodesPage />} />
+      <Route path="/episodes/:id" element={<EpisodePage />} />
       <Route path="/404" element={<NotFoundPage />} />
       <Route path="*" element={<Navigate to="/404" />} />
     </Routes>
