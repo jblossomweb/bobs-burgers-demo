f372962b0e9fc9b70c9e9f9b1167980523e63192
John Blossom <jblossom.web@gmail.com>
2022-03-07 10:32:48 -0700

    add some spacing on NotFoundPage

--- src/app/pages/NotFoundPage/NotFoundPage.tsx
+++ src/app/pages/NotFoundPage/NotFoundPage.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Button from 'app/components/atoms/Button'
 import HeroTemplate from 'app/templates/HeroTemplate'
+import styles from './NotFoundPage.module.css'
 
 const NotFoundPage: React.FC = () => (
   <HeroTemplate title="404: Not Found">
-    <h1>Sorry! That page was not found.</h1>
+    <h1 className={styles.header}>Sorry! That page was not found.</h1>
     <p>
       <Link to="/">
         <Button>Back to Home</Button>
