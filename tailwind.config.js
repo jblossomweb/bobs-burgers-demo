6a512de4bd2fc842ce3cde9ab131f7f96c4cc2c4
John Blossom <jblossom.web@gmail.com>
2022-03-07 10:55:25 -0700

    add fadein animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ const colors = require('./src/app/colors.json')
 module.exports = {
   content: ['./src/app/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: { colors },
+    extend: {
+      colors,
+      keyframes: {
+        fadein: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+      },
+      animation: {
+        fadein: 'fadein 0.333s',
+      },
+    },
   },
   plugins: [],
 }
