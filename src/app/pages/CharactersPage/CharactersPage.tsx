1b829053d4f2ad6b6184a1e3df545defa6e837fd
John Blossom <jblossom.web@gmail.com>
2022-03-07 10:33:23 -0700

    wire up CharactersPage

--- src/app/pages/CharactersPage/CharactersPage.tsx
+++ src/app/pages/CharactersPage/CharactersPage.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import HeroTemplate from 'app/templates/HeroTemplate'
+import ContentTemplate from 'app/templates/ContentTemplate'
+import useAllCharacters from 'app/hooks/useAllCharacters'
+import CharactersList from 'app/components/organisms/CharactersList'
 
-const CharactersPage: React.FC = () => (
-  <HeroTemplate title="Characters">
-    <h1>Characters</h1>
-  </HeroTemplate>
-)
+const CharactersPage: React.FC = () => {
+  const { data, loading, error } = useAllCharacters()
+  return (
+    <ContentTemplate title="Characters" loading={loading}>
+      {!!loading && <div>loading...</div>}
+      {!!error && <div>{error.message}</div>}
+      {!!data && <CharactersList characters={data} />}
+    </ContentTemplate>
+  )
+}
 
 export default CharactersPage
